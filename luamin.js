3257bde2bb45ebf86b2440ad9c9e2ffe0c375744
Mathias Bynens <mathias@qiwi.be>
2013-04-19 21:48:18 +0200

    Tag the v0.2.5 release

--- luamin.js
+++ luamin.js
@@ -1,4 +1,4 @@
-/*! http://mths.be/luamin v0.2.4 by @mathias */
+/*! http://mths.be/luamin v0.2.5 by @mathias */
 ;(function(root) {
 
 	// Detect free variables `exports`
@@ -577,7 +577,7 @@
 	/*--------------------------------------------------------------------------*/
 
 	var luamin = {
-		'version': '0.2.4',
+		'version': '0.2.5',
 		'minify': minify
 	};
 
