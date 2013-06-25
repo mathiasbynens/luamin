6e2f2f44b372f33c11d876a425f67a16800df2dc
Mathias Bynens <mathias@qiwi.be>
2013-06-25 15:10:03 +0200

    Gruntfile: Rename `travis` task

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-shell');
 
 	grunt.registerTask('cover', 'shell:cover');
-	grunt.registerTask('travis', [
+	grunt.registerTask('ci', [
 		'shell:test-narwhal',
 		'shell:test-phantomjs',
 		'shell:test-rhino',
@@ -50,7 +50,7 @@ module.exports = function(grunt) {
 		'shell:test-node',
 	]);
 	grunt.registerTask('test', [
-		'travis',
+		'ci',
 		'shell:test-browser'
 	]);
 
