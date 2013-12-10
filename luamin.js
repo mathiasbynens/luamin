1b569d08b80b3e8fea5ba3679c6ddb34accc2e0c
Oskar Schöldström <public@oxy.fi>
2013-12-10 13:57:35 +0700

    Preserve `TableKeyString` identifiers
    Closes #17 and #18.

--- luamin.js
+++ luamin.js
@@ -344,8 +344,10 @@
 				} else if (field.type == 'TableValue') {
 					result += formatExpression(field.value);
 				} else { // at this point, `field.type == 'TableKeyString'`
-					result += formatExpression(field.key) + '=' +
-						formatExpression(field.value);
+					result += formatExpression(field.key, {
+						// TODO: keep track of nested scopes (#18)
+						'preserveIdentifiers': true
+					}) + '=' + formatExpression(field.value);
 				}
 				if (needsComma) {
 					result += ',';
